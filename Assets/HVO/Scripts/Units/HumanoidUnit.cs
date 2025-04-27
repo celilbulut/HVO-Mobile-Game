b81840c9ff80c8692407e09bb44ddca4fba36651
celilbulut <celilbl12@gmail.com>
2025-04-27 01:54:20 -0400

    Apply Speed to Animator

--- Assets/HVO/Scripts/Units/HumanoidUnit.cs
+++ Assets/HVO/Scripts/Units/HumanoidUnit.cs
@@ -4,6 +4,7 @@ public class HumanoidUnit : Unit
 {
     protected Vector2 m_Velocity;
     protected Vector3 m_LastPosition;
+    public float CurrentSpeed => m_Velocity.magnitude;
 
     protected void Update()
     {
@@ -15,5 +16,7 @@ public class HumanoidUnit : Unit
 
         m_LastPosition = transform.position;
         IsMoving = m_Velocity.magnitude > 0;
+
+        m_Animator.SetFloat("Speed", Mathf.Clamp01(CurrentSpeed));
     }
 }
--- Assets/HVO/Scripts/Units/Unit.cs
+++ Assets/HVO/Scripts/Units/Unit.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 public abstract class Unit : MonoBehaviour
 {
     public bool IsMoving;
-    
+
+    protected Animator m_Animator;
+    protected void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+
+        if (TryGetComponent<Animator>(out var animator))
+        {
+            m_Animator = animator;
+        }
+    }
+
 }
