03e02b21206ce116eddf6594ac49dee393356d29
celilbulut <celilbl12@gmail.com>
2025-04-29 02:22:59 -0400

    Fadeout Effect

--- Assets/HVO/Scripts/UI/PointToClick.cs
+++ Assets/HVO/Scripts/UI/PointToClick.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PointToClick : MonoBehaviour
 {
     [SerializeField] private float m_Duration = 1f;
+    [SerializeField] private SpriteRenderer m_SpriteRenderer;
 
     private float m_Timer;
 
@@ -14,6 +15,12 @@ public class PointToClick : MonoBehaviour
     void Update()
     {
         m_Timer += Time.deltaTime;
+
+        if(m_Timer >= m_Duration * 0.9f)
+        {
+            float fadeProgress = (m_Timer - m_Duration * 0.9f) / (m_Duration * 0.1f);
+            m_SpriteRenderer.color = new Color(1, 1, 1, 1 - fadeProgress); // last 0.1 saniyede PointClick imizin renderi azalacak
+        }
 
         if(m_Timer >= m_Duration)
         {
