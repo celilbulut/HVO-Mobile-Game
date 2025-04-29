b46237109102d8ed11f51149f2246968551a6c4e
celilbulut <celilbl12@gmail.com>
2025-04-29 03:06:50 -0400

    Change Built Target

--- Assets/HVO/Scripts/Managers/GameManager.cs
+++ Assets/HVO/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameManager : SingletonManager<GameManager>
@@ -58,8 +59,11 @@ public class GameManager : SingletonManager<GameManager>
 
     void HandleClickOnGround(Vector2 worldPoint)
     {
-        DisplayClickEffect(worldPoint);
-        ActiveUnit.MoveTo(worldPoint);
+        if(HasActiveUnit && IsHumanoid(ActiveUnit))
+        {
+            DisplayClickEffect(worldPoint);
+            ActiveUnit.MoveTo(worldPoint);
+        }
     }
 
     void HandleClickOnUnit(Unit unit)
@@ -76,6 +80,11 @@ public class GameManager : SingletonManager<GameManager>
 
         ActiveUnit = unit;
         ActiveUnit.Select();
+    }
+
+    bool IsHumanoid(Unit unit)
+    {
+        return unit is HumanoidUnit;
     }
 
     void DisplayClickEffect(Vector2 worldPoint)
