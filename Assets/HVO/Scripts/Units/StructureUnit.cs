aa6cc8d61a1ccdfc4b1da679aad7dd42ec830ca1
celilbulut <celilbl12@gmail.com>
2025-05-11 01:51:54 -0400

    Building process update

--- Assets/HVO/Scripts/Units/StructureUnit.cs
+++ Assets/HVO/Scripts/Units/StructureUnit.cs
@@ -1,6 +1,19 @@
 
-
 public class StructureUnit : Unit
 {
+    private BuildingProcess m_BuildingProcess;
+    public bool IsUnderConstruction => m_BuildingProcess != null;
     
+    void Update()
+    {
+        if (IsUnderConstruction)
+        {
+            m_BuildingProcess.Update();
+        }
+    }
+
+    public void RegisterProcess(BuildingProcess process)
+    {
+        m_BuildingProcess = process;
+    }
 }
