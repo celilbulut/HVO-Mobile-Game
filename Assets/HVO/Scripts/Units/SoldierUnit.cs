e71c2452e010f6d4861718fff70be75a518f3706
celilbulut <celilbl12@gmail.com>
2025-07-06 14:26:29 -0400

    React to attack task
    Saldırı sistemini daha modüler ve ölçeklenebilir hale getirmek için çok akıllıca bir adım içeriyordu. Artık “saldırı görevleri” doğrudan SoldierUnit tarafından işleniyor. İşte bu dersin özeti:
    
    🧭 Video Özeti: Göreve Göre Hareket (MoveTo) – Görev Yönetimi İyileştirmesi
    
    🎯 Amaç:
    GameManager içindeki doğrudan MoveTo() çağrısını kaldırıp, görevi (attack) ayarlayarak bu davranışı birimin kendisine bırakmak.

--- Assets/HVO/Scripts/Units/SoldierUnit.cs
+++ Assets/HVO/Scripts/Units/SoldierUnit.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
 public class SoldierUnit : HumanoidUnit
-{
-    
+{    
+    protected override void OnSetTask(UnitTask oldTask, UnitTask newTask)
+    {
+        if (newTask == UnitTask.Attack && HasTarget)
+        {
+            MoveTo(Target.transform.position);
+        }
+
+        base.OnSetTask(oldTask, newTask);
+    }
 }
