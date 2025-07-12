ae2b884577c328faa05cfe4245ea56b72d5675e8
celilbulut <celilbl12@gmail.com>
2025-07-11 23:01:05 -0400

    Spawn Projectile
    Tower (kule) sistemine giriş yapıldı ve ilk adım olarak projectile (ok/mermi) sistemi kuruldu. İşte kısa ve net bir özet:
    
    🧱 Ders Özeti – Tower Projectile Sistemi (Ok Atışı Başlangıcı)
    
    🎯 Hedef:
    Kule, düşmanlara otomatik olarak ok atabilsin.

--- Assets/HVO/Scripts/Units/TowerUnit.cs
+++ Assets/HVO/Scripts/Units/TowerUnit.cs
@@ -1,6 +1,18 @@
-
+using UnityEngine;
 
 public class TowerUnit : StructureUnit
 {
-    
+    [SerializeField] private Projectile m_ProjectilePrefab;
+
+    public override void OnConstructionFinished()
+    {
+        base.OnConstructionFinished();
+
+        var Projectile = Instantiate(m_ProjectilePrefab, transform.position, Quaternion.identity);
+    }
+
+    protected override void AfterConstructionUpdate()
+    {
+        Debug.Log("AfterConstructionUpdate!");
+    }
 }
