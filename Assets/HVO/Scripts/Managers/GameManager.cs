0057283070f449d171e8d1a7d75e292e2391e7ad
celilbulut <celilbl12@gmail.com>
2025-07-19 18:42:52 -0400

    Update Resources in UI
    UI Üzerinden Kaynak (Gold/Wood) Sayılarının Güncellenmesi
    
    🎯 Bu Derste Hedef:
    🎯 Oyun sırasında toplanan altın (gold) ve odun (wood) miktarlarının:
    
    Üst sağ köşedeki UI'da dinamik olarak güncellenmesi
    
    Toplama veya harcama işlemlerine göre animasyonsuz, anlık yansıtılması

--- Assets/HVO/Scripts/Managers/GameManager.cs
+++ Assets/HVO/Scripts/Managers/GameManager.cs
@@ -21,6 +21,7 @@ public class GameManager : SingletonManager<GameManager>
     [SerializeField] private ActionBar m_ActionBar;
     [SerializeField] private ConfirmationBar m_BuildConfirmationBar;
     [SerializeField] private TextPopupController m_TextPopupController;
+    [SerializeField] private ResourceDataUI m_ResourceDataUI;
 
     [Header("Camera Settings")]
     [SerializeField] private float m_PanSpeed = 100;
@@ -43,8 +44,8 @@ public class GameManager : SingletonManager<GameManager>
     private CameraController m_CameraController;
     private PlacementProcess m_PlacementProcess;
 
-    private int m_Gold = 1000;
-    private int m_Wood = 1000;
+    private int m_Gold = 0;
+    private int m_Wood = 0;
     public int Gold => m_Gold;
     public int Wood => m_Wood;
 
@@ -54,6 +55,7 @@ public class GameManager : SingletonManager<GameManager>
     {
         m_CameraController = new CameraController(m_PanSpeed, m_MobilePanSpeed);
         ClearActionBarUI();
+        AddResources(500, 500);
     }
 
     void Update()
@@ -127,6 +129,8 @@ public class GameManager : SingletonManager<GameManager>
     {
         m_Gold += gold;
         m_Wood += wood;
+
+        m_ResourceDataUI.UpdateResourceDisplay(m_Gold, m_Wood);
     }
 
     public void ShowTextPopup(string text, Vector3 position, Color color)
@@ -481,14 +485,8 @@ public class GameManager : SingletonManager<GameManager>
         }
         else
         {
-            RevertResources(m_PlacementProcess.GoldCost, m_PlacementProcess.WoodCost);
+            AddResources(m_PlacementProcess.GoldCost, m_PlacementProcess.WoodCost);
         }
-    }
-
-    void RevertResources(int gold, int wood)
-    {
-        m_Gold += gold;
-        m_Wood += wood;
     }
 
     void CancelBuildPlacement()
@@ -503,18 +501,15 @@ public class GameManager : SingletonManager<GameManager>
     {
         if (m_Gold >= goldCost && m_Wood >= woodCost)
         {
-            m_Gold -= goldCost;
-            m_Wood -= woodCost;
+            AddResources(-goldCost, -woodCost);
             return true;
         }
+
         return false;
     }
 
     void OnGUI()
     {
-        GUI.Label(new Rect(50, 40, 200, 20), "Gold: " + m_Gold.ToString(), new GUIStyle { fontSize = 40 });
-        GUI.Label(new Rect(50, 80, 200, 20), "Wood: " + m_Wood.ToString(), new GUIStyle { fontSize = 40 });
-
         if (ActiveUnit != null)
         {
             GUI.Label(new Rect(50, 120, 200, 20), "State: " + ActiveUnit.CurrentState.ToString(), new GUIStyle { fontSize = 40 });
