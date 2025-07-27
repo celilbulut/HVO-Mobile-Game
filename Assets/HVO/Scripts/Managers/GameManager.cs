0a6ee580f530ca12722558bcb6faca38e702a3eb
celilbulut <celilbl12@gmail.com>
2025-07-27 04:06:01 -0400

    Game over script
    Game Over UI - Fonksiyonellik Ekleme (Unity C#)
    
    🎯 Dersin Amacı:
    Oyuncu oyunu kazandığında veya kaybettiğinde ekranda görünen "Victory" veya "Defeat" mesajlarını ve butonları aktif hale getirip, bu butonlara basıldığında neler olacağını tanımlamak.

--- Assets/HVO/Scripts/Managers/GameManager.cs
+++ Assets/HVO/Scripts/Managers/GameManager.cs
@@ -28,6 +28,7 @@ public class GameManager : SingletonManager<GameManager>
     [SerializeField] private ConfirmationBar m_BuildConfirmationBar;
     [SerializeField] private TextPopupController m_TextPopupController;
     [SerializeField] private ResourceDataUI m_ResourceDataUI;
+    [SerializeField] private GameOverLayout m_GameOverLayout;
 
     [Header("Camera Settings")]
     [SerializeField] private float m_PanSpeed = 100;
@@ -676,7 +677,8 @@ public class GameManager : SingletonManager<GameManager>
             AudioManager.Get().PlayMusic(m_LoseAudioSettings);
         }
 
-        Time.timeScale = 0; // Butun oyun duracal
+        Time.timeScale = 0; // Butun oyun duracak
+        m_GameOverLayout.ShowGameOver(isVictory);
         m_GameState = GameState.Paused;
     }
 
