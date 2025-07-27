6a07e4c85c52f2e393ab2551500057b2585eaae9
celilbulut <celilbl12@gmail.com>
2025-07-27 02:50:19 -0400

    Menu scene controls
    Menü Sahnesi Buton Kontrolü ve Arka Plan Müziği
    
    🎯 Dersin Amacı:
    Menü sahnesine Play (Oyna) ve Exit (Çıkış) butonlarını eklemek
    
    Play butonuna basıldığında oyun sahnesine geçiş yapmak
    
    Exit butonuna basıldığında oyunu kapatmak
    
    Menüye özel bir arka plan müziği çalmak

---
+++ Assets/HVO/Scripts/UI/HomeScreen.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HomeScreen : MonoBehaviour
+{
+    [SerializeField] private AudioSettings m_MenuBackgroundAudioSettings;
+    [SerializeField] private Button m_PlayButton;
+    [SerializeField] private Button m_ExitButton;
+
+    void OnEnable()
+    {
+        m_PlayButton.onClick.AddListener(OnPlayButtonClicked);
+        m_ExitButton.onClick.AddListener(OnExitButtonClicked);
+    }
+
+    void OnDisable()
+    {
+        m_PlayButton.onClick.RemoveListener(OnPlayButtonClicked);
+        m_ExitButton.onClick.RemoveListener(OnExitButtonClicked);
+    }
+
+    void Start()
+    {
+        AudioManager.Get().PlayMusic(m_MenuBackgroundAudioSettings);
+    }
+
+    void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene"); // Scene adi dogru olmali.
+    }
+
+    void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
